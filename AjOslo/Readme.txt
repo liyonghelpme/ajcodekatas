556f92d9871ba9bc4679592431a6be9bb59b740e
webmaster <webmaster@ajlopez.com>
2009-04-06 02:09:01 +0000

    Initial Project in ajcodekatas Lexer (borrowed from AjRuby) First test

---
+++ AjOslo/Readme.txt
@@ -0,0 +1,9 @@
+
+AjOslo
+
+Implementation of Oslo ideas. First one to implement: MGrammar parsing
+
+By Angel "Java" Lopez
+http://www.ajlopez.com/en
+http://ajlopez.wordpress.com
+
---
+++ AjOslo/Src/AjOslo.MGrammar.Tests/LexerTest.cs
@@ -0,0 +1,31 @@
+﻿namespace AjOslo.MGrammar.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using AjOslo.MGrammar.Compiler;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LexerTest
+    {
+        [TestMethod]
+        public void ShouldParseName()
+        {
+            Lexer lexer = new Lexer("name");
+
+            Token token = lexer.NextToken();
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual(TokenType.Name, token.TokenType);
+            Assert.AreEqual("name", token.Value);
+
+            token = lexer.NextToken();
+
+            Assert.IsNull(token);
+        }
+    }
+}
