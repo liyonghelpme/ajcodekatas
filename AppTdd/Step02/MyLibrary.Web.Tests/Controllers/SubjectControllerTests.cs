749d1cfca665f067a88915caa22a9e649bdaa11e
webmaster <webmaster@ajlopez.com>
2011-03-26 22:03:12 +0000

    AppTdd Step02

---
+++ AppTdd/Step02/MyLibrary.Web.Tests/Controllers/SubjectControllerTests.cs
@@ -0,0 +1,65 @@
+﻿namespace MyLibrary.Web.Tests.Controllers
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Web.Mvc;
+    using MyLibrary.Web.Models;
+    using MyLibrary.Web.Controllers;
+
+    [TestClass]
+    public class SubjectControllerTests
+    {
+        [TestMethod]
+        public void GetSubjectsInIndex()
+        {
+            IEnumerable<Subject> subjects = GetSubjects();
+            SubjectController controller = new SubjectController(subjects);
+
+            ActionResult result = controller.Index();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+            ViewResult viewResult = (ViewResult)result;
+
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(IList<Subject>));
+            Assert.AreSame(subjects, viewResult.ViewData.Model);
+        }
+
+        [TestMethod]
+        public void GetSubjectInDetail()
+        {
+            IEnumerable<Subject> subjects = GetSubjects();
+            SubjectController controller = new SubjectController(subjects);
+
+            ActionResult result = controller.Details(1);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+            ViewResult viewResult = (ViewResult)result;
+
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(Subject));
+
+            Subject model = (Subject)viewResult.ViewData.Model;
+            Assert.AreEqual(1, model.Id);
+            Assert.AreEqual("Mathematics", model.Name);
+        }
+
+        private static IEnumerable<Subject> GetSubjects()
+        {
+            IEnumerable<Subject> subjects = new List<Subject>()
+            {
+                new Subject() { Id = 1, Name = "Mathematics" },
+                new Subject() { Id = 2, Name = "Physics" },
+                new Subject() { Id = 3, Name = "Biology" },
+                new Subject() { Id = 4, Name = "Literature" }
+            };
+
+            return subjects;
+        }
+    }
+}
---
+++ AppTdd/Step02/MyLibrary.Web/Controllers/SubjectController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyLibrary.Web.Models;
+
+namespace MyLibrary.Web.Controllers
+{
+    public class SubjectController : Controller
+    {
+        private IEnumerable<Subject> subjects;
+
+        public SubjectController()
+        {
+        }
+
+        public SubjectController(IEnumerable<Subject> subjects)
+        {
+            this.subjects = subjects;
+        }
+
+        public ActionResult Index()
+        {
+            return View(subjects);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var model = this.subjects.Where(s => s.Id == id).FirstOrDefault();
+            return View(model);
+        }
+    }
+}
