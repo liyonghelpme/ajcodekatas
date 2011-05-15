0f32208391ec114d30efbd84d4656eb89f208430
webmaster <webmaster@ajlopez.com>
2011-05-15 00:17:19 +0000

    NHibernate, new example

---
+++ NHibernate/BooksChapters/Src/Books.Console/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Books.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using NHibernate;
+    using NHibernate.Cfg;
+    using NHibernate.Linq;
+    using Books;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISessionFactory sessionFactory = new Configuration().Configure().BuildSessionFactory();
+
+            using (ISession session = sessionFactory.OpenSession())
+            {
+                using (ITransaction tx = session.BeginTransaction())
+                {
+                    Book cookbook = new Book()
+                    {
+                        Title = "NHibernate Cookbook",
+                        Author = "Jason Dentler",
+                        Chapters = new List<Chapter>()
+                    };
+
+                    cookbook.Chapters.Add(new Chapter() { Title = "Models and Mappings", Book = cookbook });
+                    cookbook.Chapters.Add(new Chapter() { Title = "Configuration and Schema", Book = cookbook });
+                    cookbook.Chapters.Add(new Chapter() { Title = "Configuration and Schema", Book = cookbook });
+
+                    session.Save(cookbook);
+
+                    foreach (Book book in session.Query<Book>().Fetch(b => b.Chapters))
+                    {
+                        System.Console.WriteLine(string.Format("Book {0}", book.Title));
+                        int nchapter = 0;
+                        foreach (Chapter chapter in book.Chapters)
+                            System.Console.WriteLine(string.Format("Chapter {0}:{1}", ++nchapter, chapter.Title));
+                    }
+
+                    tx.Commit();
+                    session.Close();
+                }
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
--- NHibernate/BooksChapters/Src/Books/Properties/AssemblyInfo.cs
+++ NHibernate/BooksChapters/Src/Books/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BooksOneToMany")]
+[assembly: AssemblyTitle("Books")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BooksOneToMany")]
+[assembly: AssemblyProduct("Books")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("640239b0-f32e-4da0-b144-68317005a25c")]
+[assembly: Guid("39089c2b-c2da-4750-97dd-055c9fc19559")]
 
 // Version information for an assembly consists of the following four values:
 //
