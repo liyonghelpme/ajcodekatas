b4f767f224a55ec30563bf84b9349925054d0d59
webmaster <webmaster@ajlopez.com>
2011-06-16 11:15:21 +0000

    AjModel New class diagrams for post http://ajlopez.wordpress.com/2011/06/16/ajmodel-model-by-code-part-1-inception/ New classes to use in post

---
+++ AjModel/Src/AjModel.Tests/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AjModel.Tests.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public IList<OrderLine> OrderLines { get; set; }
+    }
+}
---
+++ AjModel/Src/AjModel.Tests/Entities/OrderLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AjModel.Tests.Entities
+{
+    public class OrderLine
+    {
+        public int Id { get; set; }
+
+        public Order Order { get; set; }
+
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
